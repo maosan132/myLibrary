985b945807b9ce5c9da308282e5dacd847bad066
maosan132 <maosan132@gmail.com>
2021-01-05 16:07:03 -0500

    feat(library): :sparkles: Completed submit event handler
    Completed submit event handler by adding assigning form values to script variables and send them to addBookToLibrary fx

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const getIdEl = e => document.getElementById(e)
 const getClasEl = e => document.getElementsByClassName(e)
 const newEl = e => document.createElement(e)
 const newTxt = e => document.createTextNode(e)
-const querEl = e => document.querySeector(e)
+const queSel = e => document.querySelector(e)
 
 log('main.js loaded') //?
 //work with HTML elements
@@ -16,42 +16,39 @@ const bookCreatorForm = getIdEl("collapseContent") //?
 log(bookCreatorForm) //? 
 let newCard = newEl('div')
 let newCardTitle = newEl('h4')
-let newCard
-const currentLibrary = []
-let myLibrary = [
+let newAuthor = newPages = newEl('span')
+log(newPages, newAuthor)
+let newRadioButton = newEl('input')
+newRadioButton.setAttribute('type', 'radio')
+newRadioButton.value = false
+
+
+
+
+
+const currentLibrary = [
   {
     title: "The Parfum",
     author: "Patrick Suskind",
     pages: 354,
     read: true
-  }]
+  }
+]
+let myLibrary = []
 
-const Book = function(title, author, pages, read){
+const Book = function(title, author, pages, description, read){
   this.title = title
   this.author = author
   this.pages = pages
-  this.read = read
+  this.description = description
+  this.readStatus = read
 };
 
 Book.prototype.info = () => `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
 
-const addBookToLibrary = function (book, library) {
-  library.push(book)
-  log(`${book.title} by ${book.author} was added to library
-  ${log(library)}`)
-}
-
-let secondBook = new Book('4323452345', '8757567', 24, true)
-
-//log(firstBook.title)
-addBookToLibrary(secondBook, myLibrary)
-log(myLibrary)
-
-bookCreatorForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  log('form submitted') //not refreshing index.html for data retaining purposes
-  
-})
+Book.prototype.toggleReadStatus = () {
+  this.readStatus = !this.readStatus;
+};
 
 
 // Book.prototype.updateReadState = function updateReadState() {
@@ -61,6 +58,48 @@ bookCreatorForm.addEventListener('submit', (e) => {
 //     this.read = true
 //   }
 // }
+
+const addCurrentBookToLibrary = () => {
+  
+}
+
+const addBookToLibrary = function (book, library) {
+  library.push(book)
+  log(`${book.title} by ${book.author} was added to library
+  ${log(library)}`)
+}
+
+// const removeBookFromLibrary = function (book, library) {
+//   library.push(book)
+//   log(`${book.title} by ${book.author} was added to library
+//   ${log(library)}`)
+// }
+
+let secondBook = new Book('4323452345', '8757567', 24, true)
+
+//log(firstBook.title)
+addBookToLibrary(secondBook, myLibrary)
+log(myLibrary)
+
+
+//Update page with lastest books
+
+
+
+//submit book event handler
+bookCreatorForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  log('form submitted') //not refreshing index.html for data retaining purposes
+  const newTitle = queSel("#title").value
+  const newAuthor = queSel("#author").value
+  const newPages = queSel("#pages").value
+  const newDescription = queSel("#description").value
+  const newRead = queSel("#read").value
+  let newBook = new Book(newTitle, newAuthor, newPages, newDescription, newRead)
+  addBookToLibrary(newBook)
+  updateBookView()
+})
+
 
 // const name = querySelect("#name");
 // const author = querySelect("#author");
