7a462c53c953842ea768cab45fb0aaad404723bb
maosan132 <maosan132@gmail.com>
2021-01-05 22:46:59 -0500

    solved a problem that prevents from append a child, it was produced by a bad var assignment, also removed useless comments

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
 /* 
 Main file for My Library project
-
-First 6 expressions are shorter versions of commonly used functions: console.log, etc. Use it at will
 */
-// For testing purposes
+//        For testing purposes
 
 const log = e => console.log(e) 
 const check1 = () => console.log('---------- check -----------')
 const check2 = () => console.log('********** check ***********')
-// const getIdEl = e => document.getElementById(e)
-// const getClasEl = e => document.getElementsByClassName(e)
-// const newEl = e => document.createElement(e)
-// const newTxt = e => document.createTextNode(e)
-// const queSel = e => document.querySelector(e)
 
-
-//Work with HTML predefined elements
+//        Work with HTML predefined elements
 const bookCreatorForm = document.getElementById('collapseContent') //? 
 const libraryHolder = document.querySelector('.card-group')
 const bookHolder = document.querySelector('.card')
-const newCard = newCardBody = document.createElement('div')
+const newCard = document.createElement('div')
+const newCardBody = document.createElement('div')
 const newBookTitle = document.createElement('h4')
-const newAuthor = newPages = document.createElement('p')
+const newAuthor =document.createElement('p')
+const newPages = document.createElement('p')
+const newDescription = document.createElement('p')
 const newCheckbox = document.createElement('input')
 const deleteButton = document.createElement('button')
 
-//Initial library
+//      Initial library
 const currentLibrary = [
   {
     title: 'The Parfum',
@@ -47,7 +42,8 @@ const Book = function(title, author, pages, description, read){
 
 //      Prototyping both info and read status toggler functions for Book
 
-Book.prototype.info = () => `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+Book.prototype.info = () => `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus}`
+
 Book.prototype.toggleReadStatus = () =>{
   this.readStatus = !this.readStatus;
 };
@@ -71,12 +67,6 @@ const addBookToLibrary = function (book, library = myLibrary) {
   ${log(library)}`)
 }
 
-// Const removeBookFromLibrary = function (book, library) {
-//   library.push(book)
-//   log(`${book.title} by ${book.author} was added to library
-//   ${log(library)}`)
-// }
-
 let secondBook = new Book('4323452345', '8757567', 24, true)
 
 //log(firstBook.title)
@@ -92,7 +82,7 @@ log(myLibrary)
 
 bookCreatorForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  log('form submitted') //not refreshing index.html for data retaining purposes
+  log('form submitted')
   const newTitle = document.querySelector('#title').value
   const newAuthor = document.querySelector('#author').value
   const newPages = document.querySelector('#pages').value
@@ -106,7 +96,12 @@ bookCreatorForm.addEventListener('submit', (e) => {
   updateBookView()
 })
 
-
+//        Delete button event handler
+deleteButton.addEventListener('click', () => {
+  const index = myLibrary.findIndex(i => i === book)
+  myLibrary.splice(index, 1)
+  updateBookView()
+})
 
 //        Renders all books in page
 
@@ -127,9 +122,13 @@ const updateBookView = () => {
     
     // Add data to new bootstrap card elements
     newBookTitle.textContent = book.title
+    log('title:' + book.title)
     newAuthor.textContent = book.author
+    log('author:' + book.author)
     newPages.textContent = book.pages
+    log('pages:' + book.pages)
     newDescription.textContent = book.description
+    log('descripton:' + book.description)
     newCheckbox.value = false
     deleteButton.textContent = 'Delete'
 
@@ -156,12 +155,6 @@ const updateBookView = () => {
     //     console.log(color);
     // }
 
-
-
-
-
-    document.querySelector('#books').appendChild(divEl)
-
     document.querySelector('#title').value = ''
     document.querySelector('#author').value = ''
     document.querySelector('#pages').value = ''
@@ -170,19 +163,4 @@ const updateBookView = () => {
 
 }
 
-// const name = querySelect('#name');
-// const author = querySelect('#author');
-// const pages = querySelect('#pages');
-// const read = querySelect('#read');
-// const form = querySelect('form').addEventListener('submit', function(e) {  
-//   addBookToLibrary();
-// });
-
-// function addBookToLibrary() {
-//   // do stuff here
-// }
-
-// function render(books) {
-
-// }
 
